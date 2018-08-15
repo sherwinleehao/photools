d597048cb5fc6c8e7222984e0ad511a95e82e142
sherwinleehao <sherwin_leehao@163.com>
2018-08-16 00:19:29 +0800

    Test the custom widget

--- QT_Custom_Widget.py
+++ QT_Custom_Widget.py
@@ -1,14 +1,80 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+Image Tool Library
+
+Author: Sherwin Lee
+Website: Sherwinleehao.com
+Last edited: 20180815
+"""
+
+
+#
+#
+#
+
+
 import sys
-from PyQt5.QtWidgets import (QWidget, QSlider, QApplication, QHBoxLayout, QVBoxLayout)
-from PyQt5.QtCore import QObject, Qt, pyqtSignal
-from PyQt5.QtGui import QPainter, QFont, QColor, QPen
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+import cv2
+import numpy as np
+import time
+import sip
+from photools import *
 
 
-class Communicate(QObject):
-    updateBW = pyqtSignal(int)
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.info = QLabel("Hello world :)")
+        self.ssld = SuperSlider()
+        # self.c.updateBW[int].connect(self.wid.setValue)
+        # sld.valueChanged[int].connect(self.changeValue)
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.ssld)
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.info)
+        vbox.addStretch(1)
+        vbox.addLayout(hbox)
+        vbox.addLayout(hbox)
+        self.setLayout(vbox)
+
+        self.setGeometry(300, 200, 640, 150)
+        self.setWindowTitle('Custom Widget')
+        self.show()
 
 
-class BurningWidget(QWidget):
+class SuperSlider(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setMinimumSize(1, 30)
+        self.value = 75
+
+    def paintEvent(self, e):
+        qp = QPainter()
+        qp.begin(self)
+        self.drawWidget(qp)
+        qp.end()
+
+    def drawWidget(self, qp):
+        # font = QFont("Serif", 7, QFont.Light)
+        # qp.setFont(font)
+        qp.drawRect(0, 0, 200, 20)
+        qp.drawLine(0, 0, 200, 20)
+
+
+
+class SuperSlider2(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
@@ -67,41 +133,7 @@ class BurningWidget(QWidget):
             qp.drawText(i-fw/2, h/2, str(self.num[j]))
             j = j + 1
 
-
-class Example(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-
-    def initUI(self):
-        sld = QSlider(Qt.Horizontal, self)
-        sld.setFocusPolicy(Qt.NoFocus)
-        sld.setRange(1, 750)
-        sld.setValue(75)
-        sld.setGeometry(30, 40, 550, 30)
-
-        self.c = Communicate()
-        self.wid = BurningWidget()
-        self.c.updateBW[int].connect(self.wid.setValue)
-
-        sld.valueChanged[int].connect(self.changeValue)
-        hbox = QHBoxLayout()
-        hbox.addWidget(self.wid)
-        vbox = QVBoxLayout()
-        vbox.addStretch(1)
-        vbox.addLayout(hbox)
-        self.setLayout(vbox)
-
-        self.setGeometry(300, 300, 600, 210)
-        self.setWindowTitle("Burning widget")
-        self.show()
-
-    def changeValue(self, value):
-        self.c.updateBW.emit(value)
-        self.wid.repaint()
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Example()
     sys.exit(app.exec_())
