0abd1c9789147264b0dfcfa4495fe55e091b7fa2
sherwinleehao <sherwin_leehao@163.com>
2018-08-22 00:44:14 +0800

    add a list of thumbnails

--- Reference/QT5_Pre-research.py
+++ Reference/QT5_Pre-research.py
@@ -33,33 +33,31 @@ class QQ(QToolBox):
     def __init__(self):
         super().__init__()
         pListView = ListView()
-
         pListView.setViewMode(QListView.ListMode)
         pListView.setStyleSheet("QListView{icon-size:70px}")
         dic_list = {'listview': pListView, 'groupname': "我的好友"}
         pListView.setListMap(dic_list)
         self.addItem(pListView, "我的好友")
-        print(pListView.map_listview)
         self.show()
 
-    # def contextMenuEvent(self, event):
-    #     pmenu = QMenu(self)
-    #     pAddGroupAct = QAction("添加分组", pmenu)
-    #     pmenu.addAction(pAddGroupAct)
-    #     pAddGroupAct.triggered.connect(self.addGroupSlot)
-    #     pmenu.popup(self.mapToGlobal(event.pos()))
-    #
-    # def addGroupSlot(self):
-    #     groupname = QInputDialog.getText(self, "输入分组名", "")
-    #     if groupname[0] and groupname[1]:
-    #         pListView1 = ListView()
-    #         pListView1.setViewMode(QListView.ListMode)
-    #         pListView1.setStyleSheet("QListView{icon-size:70px}")
-    #         self.addItem(pListView1, groupname[0])
-    #         dic_list = {'listview': pListView1, 'groupname': groupname[0]}
-    #         pListView1.setListMap(dic_list)
-    #     elif groupname[0] == '' and groupname[1]:
-    #         QMessageBox.warning(self, "警告", "我说你没有填写分组名哦~！")
+    def contextMenuEvent(self, event):
+        pmenu = QMenu(self)
+        pAddGroupAct = QAction("添加分组", pmenu)
+        pmenu.addAction(pAddGroupAct)
+        pAddGroupAct.triggered.connect(self.addGroupSlot)
+        pmenu.popup(self.mapToGlobal(event.pos()))
+
+    def addGroupSlot(self):
+        groupname = QInputDialog.getText(self, "输入分组名", "")
+        if groupname[0] and groupname[1]:
+            pListView1 = ListView()
+            pListView1.setViewMode(QListView.ListMode)
+            pListView1.setStyleSheet("QListView{icon-size:70px}")
+            self.addItem(pListView1, groupname[0])
+            dic_list = {'listview': pListView1, 'groupname': groupname[0]}
+            pListView1.setListMap(dic_list)
+        elif groupname[0] == '' and groupname[1]:
+            QMessageBox.warning(self, "警告", "我说你没有填写分组名哦~！")
 
 
 class ListView(QListView):
@@ -118,7 +116,6 @@ class ListModel(QAbstractListModel):
         super().__init__()
         self.ListItemData = []
         self.Data_init()
-        print(self.ListItemData)
 
     def data(self, index, role):
         if index.isValid() or (0 <= index.row() < len(self.ListItemData)):
@@ -144,13 +141,15 @@ class ListModel(QAbstractListModel):
 
 
     def Data_init(self):
-        randomnum = random.sample(range(100), 30)
+        randomnum = random.sample(range(26), 10)
         for i in randomnum:
-            randname = str("Sherwin %d"%i)
-            ItemData = {'name': '', 'iconPath': ''}
-            ItemData['name'] = randname
-            ItemData['iconPath'] = "img/thumb/thumb_%04d.jpg"%i
-            self.ListItemData.append(ItemData)
+            # randname = Random_Name.getname()
+            # randname = str(uuid.uuid4())
+            randname = str("Sherwin lee")
+        ItemData = {'name': '', 'iconPath': ''}
+        ItemData['name'] = randname
+        ItemData['iconPath'] = "./res/" + str(i) + ".jpg"
+        self.ListItemData.append(ItemData)
 
 
     def addItem(self, itemData):
@@ -185,14 +184,16 @@ class Example(QWidget):
         self.setGeometry(100, 100, 360, 720)
         self.setWindowTitle("Pre-Research")
 
+
+
         self.show()
 
-
+    def mousePressEvent(self, QMouseEvent):
+        print('PPPPPPP')
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-
     ex = Example()
     sys.exit(app.exec_())
 
