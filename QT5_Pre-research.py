36948a78c69d7687b61bc8535c0a779fb8e8528f
sherwinleehao <sherwin_leehao@163.com>
2018-08-21 22:15:15 +0800

    Enable to test the example of QlistView

--- QT5_Pre-research.py
+++ QT5_Pre-research.py
@@ -31,82 +31,178 @@ from PyQt5.QtGui import QPainter, QFont, QColor, QPen
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
-import os
-
-# class Example(QWidget):
-#     def __init__(self):
-#         super().__init__()
-#         self.initUI()
-#
-#     def initUI(self):
-#         text0 = QLabel("Title0",self)
-#         text0.move(10,10)
-#         self.setGeometry(100, 100, 640, 360)
-#         self.setWindowTitle("Pre-Research")
-#
-#         self.show()
-#
-#     def mousePressEvent(self, QMouseEvent):
-#         print('PPPPPPP')
-#
-#     def dragEnterEvent(self, event):
-#         if event.mimeData().hasUrls():
-#             event.accept()
-#             print('xxxxxxxxx')
-#         else:
-#             event.ignore()
-#
-#     def dropEvent(self, event):
-#         for url in event.mimeData().urls():
-#             path = url.toLocalFile().toLocal8Bit().data()
-#             if os.path.isfile(path):
-#                 print(path)
-#
-# if __name__ == "__main__":
-#     app = QApplication(sys.argv)
-#     ex = Example()
-#     sys.exit(app.exec_())
+import os,random
+import uuid
 
 
+class QQ(QToolBox):
+    def __init__(self):
+        super().__init__()
+        pListView = ListView()
+        pListView.setViewMode(QListView.ListMode)
+        pListView.setStyleSheet("QListView{icon-size:70px}")
+        dic_list = {'listview': pListView, 'groupname': "我的好友"}
+        pListView.setListMap(dic_list)
+        self.addItem(pListView, "我的好友")
+        self.show()
 
-class Button(QPushButton):
-    def __init__(self, title, parent):
-        super().__init__(title, parent)
+    def contextMenuEvent(self, event):
+        pmenu = QMenu(self)
+        pAddGroupAct = QAction("添加分组", pmenu)
+        pmenu.addAction(pAddGroupAct)
+        pAddGroupAct.triggered.connect(self.addGroupSlot)
+        pmenu.popup(self.mapToGlobal(event.pos()))
 
-        self.setAcceptDrops(True)
+    def addGroupSlot(self):
+        groupname = QInputDialog.getText(self, "输入分组名", "")
+        if groupname[0] and groupname[1]:
+            pListView1 = ListView()
+            pListView1.setViewMode(QListView.ListMode)
+            pListView1.setStyleSheet("QListView{icon-size:70px}")
+            self.addItem(pListView1, groupname[0])
+            dic_list = {'listview': pListView1, 'groupname': groupname[0]}
+            pListView1.setListMap(dic_list)
+        elif groupname[0] == '' and groupname[1]:
+            QMessageBox.warning(self, "警告", "我说你没有填写分组名哦~！")
 
-    def dragEnterEvent(self, e):
 
-        if e.mimeData().hasFormat('text/plain'):
-            e.accept()
+class ListView(QListView):
+    map_listview = []
+
+    def __init__(self):
+        super().__init__()
+        self.m_pModel = ListModel()
+        self.setModel(self.m_pModel)
+
+    def contextMenuEvent(self, event):
+        hitIndex = self.indexAt(event.pos()).column()
+        if hitIndex > -1:
+            pmenu = QMenu(self)
+            pDeleteAct = QAction("删除", pmenu)
+            pmenu.addAction(pDeleteAct)
+            pDeleteAct.triggered.connect(self.deleteItemSlot)
+            pSubMenu = QMenu("转移联系人至", pmenu)
+            pmenu.addMenu(pSubMenu)
+            for item_dic in self.map_listview:
+                pMoveAct = QAction(item_dic['groupname'], pmenu)
+                pSubMenu.addAction(pMoveAct)
+                pMoveAct.triggered.connect(self.move)
+            pmenu.popup(self.mapToGlobal(event.pos()))
+
+    def deleteItemSlot(self):
+        index = self.currentIndex().row()
+        if index > -1:
+            self.m_pModel.deleteItem(index)
+
+    def setListMap(self, listview):
+        self.map_listview.append(listview)
+
+    def addItem(self, pitem):
+        self.m_pModel.addItem(pitem)
+
+    def move(self):
+        tolistview = self.find(self.sender().text())
+        if tolistview is self:
+            prelistview = self.sender().text()
+            QMessageBox.warning(self, "警告", "该联系人就在{}，还怎么移动啊！".format(prelistview))
         else:
-            e.ignore()
+            index = self.currentIndex().row()
+            pItem = self.m_pModel.getItem(index)
+            tolistview.addItem(pItem)
+            self.m_pModel.deleteItem(index)
 
-    def dropEvent(self, e):
+    def find(self, pmenuname):
+        for item_dic in self.map_listview:
+            if item_dic['groupname'] == pmenuname:
+                return item_dic['listview']
 
-        self.setText(e.mimeData().text())
+
+class ListModel(QAbstractListModel):
+    def __init__(self):
+        super().__init__()
+        self.ListItemData = []
+        self.Data_init()
+
+    def data(self, index, role):
+        if index.isValid() or (0 <= index.row() < len(self.ListItemData)):
+            if role == Qt.DisplayRole:
+                return QVariant(self.ListItemData[index.row()]['name'])
+            elif role == Qt.DecorationRole:
+                return QVariant(QIcon(self.ListItemData[index.row()]['iconPath']))
+            elif role == Qt.SizeHintRole:
+                return QVariant(QSize(70, 80))
+            elif role == Qt.TextAlignmentRole:
+                return QVariant(int(Qt.AlignHCenter | Qt.AlignVCenter))
+            elif role == Qt.FontRole:
+                font = QFont()
+                font.setPixelSize(20)
+                return QVariant(font)
+
+        else:
+            return QVariant()
+
+
+    def rowCount(self, parent=QModelIndex()):
+        return len(self.ListItemData)
+
+
+    def Data_init(self):
+        randomnum = random.sample(range(26), 10)
+        for i in randomnum:
+            # randname = Random_Name.getname()
+            # randname = str(uuid.uuid4())
+            randname = str("Sherwin lee")
+        ItemData = {'name': '', 'iconPath': ''}
+        ItemData['name'] = randname
+        ItemData['iconPath'] = "./res/" + str(i) + ".jpg"
+        self.ListItemData.append(ItemData)
+
+
+    def addItem(self, itemData):
+        if itemData:
+            self.beginInsertRows(QModelIndex(), len(self.ListItemData), len(self.ListItemData) + 1)
+            self.ListItemData.append(itemData)
+            self.endInsertRows()
+
+
+    def deleteItem(self, index):
+        del self.ListItemData[index]
+
+
+    def getItem(self, index):
+        if index > -1 and index < len(self.ListItemData):
+            return self.ListItemData[index]
+
 
 
 class Example(QWidget):
     def __init__(self):
         super().__init__()
-
         self.initUI()
 
     def initUI(self):
-        edit = QLineEdit('', self)
-        edit.setDragEnabled(True)
-        edit.move(30, 65)
+        text0 = QLabel("1",self)
+        text0.move(10,10)
 
-        button = Button("Button", self)
-        button.move(190, 65)
+        LM = QQ()
+        hbox = QHBoxLayout()
+        hbox.addWidget(LM)
+        self.setLayout(hbox)
+        # print(LM.data)
+        self.setGeometry(100, 100, 640, 360)
+        self.setWindowTitle("Pre-Research")
 
-        self.setWindowTitle('Simple drag and drop')
-        self.setGeometry(300, 300, 300, 150)
 
 
-if __name__ == '__main__':
+        self.show()
+
+    def mousePressEvent(self, QMouseEvent):
+        print('PPPPPPP')
+
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     ex = Example()
-    ex.show()
-    app.exec_()
+    sys.exit(app.exec_())
+
+
