44073acc76490f68daaacc36622f2d1712b61663
sherwinleehao <sherwin_leehao@163.com>
2018-08-24 23:40:39 +0800

    add AudioWaveform reference

---
+++ Reference/AudioWaveform.py
@@ -0,0 +1,18 @@
+import time
+from pydub import AudioSegment
+sound = AudioSegment.from_file("Footages/test.mp3", format="mp3")
+peak_amplitude = sound.max
+loudness = sound.dBFS
+print(loudness)
+
+st = time.time()
+
+for i in range(0,250):
+
+    # second = i*1000
+
+    segment = sound[i*1000:(i+1)*1000]
+    print('%5d'%i,segment.max)
+
+et = time.time()
+print("used time:",et-st)
