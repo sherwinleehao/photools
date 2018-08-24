be8d82062156d77d91a0d9a8f72b1dc037a01663
sherwinleehao <sherwin_leehao@163.com>
2018-08-25 01:38:41 +0800

    add the Draw wavefrom code

--- Reference/AudioWaveform.py
+++ Reference/AudioWaveform.py
@@ -1,19 +1,22 @@
 import time
 from pydub import AudioSegment
-sound = AudioSegment.from_file("Footages/test.mp3", format="mp3")
+sound = AudioSegment.from_file("D:/python/photools/Footages/test.mp3", format="mp3")
 peak_amplitude = sound.max
 loudness = sound.dBFS
 print(loudness)
 
 st = time.time()
 
-for i in range(0,250):
+wave = []
+for i in range(0,1024):
 
     # second = i*1000
 
-    segment = sound[i*1000:(i+1)*1000]
+    segment = sound[i*1000/4:(i+1)*1000/4]
     print('%5d'%i,segment.max)
-
+    wave.append([i,segment.max])
 et = time.time()
 
 print("used time:",et-st)
+
+print(wave)
