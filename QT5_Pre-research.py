e3a5bcd345d289269be45688bb02dc42db844f5d
sherwinleehao <sherwin_leehao@163.com>
2018-09-08 18:33:28 +0800

    start new pre-research

--- QT5_Pre-research.py
+++ QT5_Pre-research.py
@@ -30,12 +30,14 @@ Task:
 
 
 点击设置按钮能切换到设置页面，
-    包括：分辨率 帧率 识别间隔 人脸识别  模糊度识别  直方图识别  运动趋势识别
+    包括：分辨率 帧率 是否分析 识别间隔 人脸识别  模糊度识别  直方图识别  运动趋势识别 重设为默认值
 
 设置页面会优先读取Temp文件夹下的Json文件，反序列化为设置
 对设置的修改也会后台修改该Json文件
 
-点击分析后进入分析模式+
+点击分析后进入生成模式
+    列表名称及进度
+    暂停 停止
 
 
 
@@ -51,265 +53,82 @@ from PyQt5.QtGui import *
 import os, random, time
 import uuid, shutil
 import photools as pt
-import ctypes
 
 
-
-class ListView(QListView):
-    map_listview = []
+class MainWindow(QWidget):
 
     def __init__(self):
-        super().__init__()
-        self.m_pModel = ListModel()
-        self.setModel(self.m_pModel)
-
-    def contextMenuEvent(self, event):
-        hitIndex = self.indexAt(event.pos()).column()
-        if hitIndex > -1:
-            pmenu = QMenu(self)
-            pDeleteAct = QAction("删除", pmenu)
-            pDeleteAct.setShortcut('Del')
-            pDeleteAct.triggered.connect(self.deleteItemSlot)
-            pmenu.addAction(pDeleteAct)
-            pmenu.popup(self.mapToGlobal(event.pos()))
-
-    def deleteItemSlot(self):
-        index = self.currentIndex().row()
-        print("deleteItemSlot ", index)
-        if index > -1:
-            self.m_pModel.deleteItem(index)
-
-    def addItem(self, pitem):
-        self.m_pModel.addItem(pitem)
-
-    def removeItem(self, index):
-        self.m_pModel.deleteItem(index)
-
-    def showSelection(self):
-        print(self.currentIndex().row())
+        super(MainWindow, self).__init__()
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(TitleBar(self))
+        self.setLayout(self.layout)
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.layout.addStretch(-1)
+        self.setMinimumSize(360, 720)
+        self.setWindowFlags(Qt.FramelessWindowHint)
+        self.pressing = False
 
 
-class ListModel(QAbstractListModel):
-    def __init__(self):
-        super().__init__()
-        self.ListItemData = []
-        self.Data_init()
+class TitleBar(QWidget):
 
-    def data(self, index, role):
-        # print("data:",index.isValid(),index.row,role)
-        if index.isValid() or (0 <= index.row() < len(self.ListItemData)):
-            if role == Qt.DisplayRole:
-                # return QVariant(self.ListItemData[index.row()]['name'])
-                return QVariant(self.parseItem(self.ListItemData[index.row()]))
-            elif role == Qt.DecorationRole:
-                return QVariant(QIcon(self.ListItemData[index.row()]['iconPath']))
-            elif role == Qt.SizeHintRole:
-                return QVariant(QSize(70, 80))
-            elif role == Qt.TextAlignmentRole:
-                # return QVariant(int(Qt.AlignHCenter | Qt.AlignVCenter))
-                return QVariant(int(Qt.AlignLeft | Qt.AlignVCenter))
-            elif role == Qt.FontRole:
-                font = QFont()
-                font.setPixelSize(12)
-                return QVariant(font)
-        else:
-            return QVariant()
+    def __init__(self, parent):
+        super(TitleBar, self).__init__()
+        self.parent = parent
+        print(self.parent.width())
+        self.layout = QHBoxLayout()
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.title = QLabel("Filmora Auto Generator")
 
-    def rowCount(self, parent=QModelIndex()):
-        return len(self.ListItemData)
+        btn_size = 24
 
-    def Data_init(self):
-        print('Data_init')
-        # randomnum = random.sample(range(100), 5)
-        # for i in randomnum:
-        #     randname = str("Sherwin %d" % i)
-        #     ItemData = {'name': '', 'iconPath': ''}
-        #     ItemData['name'] = randname
-        #     ItemData['iconPath'] = "img/thumb/thumb_%04d.jpg" % i
-        #     self.ListItemData.append(ItemData)
-        #     print("ItemData", ItemData)
+        self.btn_close = QPushButton("x")
+        self.btn_close.clicked.connect(self.btn_close_clicked)
+        self.btn_close.setFixedSize(btn_size, btn_size)
+        self.btn_close.setStyleSheet("background-color: red;")
 
-    def addItem(self, itemData):
-        print('addItem')
-        if itemData:
-            self.beginInsertRows(QModelIndex(), len(self.ListItemData), len(self.ListItemData) + 1)
-            self.ListItemData.append(itemData)
-            self.endInsertRows()
+        self.title.setFixedHeight(35)
+        self.title.setAlignment(Qt.AlignCenter)
+        self.layout.addWidget(self.title)
+        # self.layout.addWidget(self.btn_min)
+        # self.layout.addWidget(self.btn_max)
+        self.layout.addWidget(self.btn_close)
 
-    def deleteItem(self, index):
-        print('deleteItem', index)
-        del self.ListItemData[index]
+        self.title.setStyleSheet("""
+            background-color: black;
+            color: white;
+        """)
+        self.setLayout(self.layout)
 
-    def getItem(self, index):
-        print('getItem', index)
-        if index > -1 and index < len(self.ListItemData):
-            return self.ListItemData[index]
+        self.start = QPoint(0, 0)
+        self.pressing = False
 
-    def parseItem(self,item):
-        name = item['name']
-        # iconPath = item['iconPath']
-        # filePath = item['filePath']
-        mediaInfo = item['mediaInfo']
-        tempStr = name+"\n"+mediaInfo
-        print(tempStr)
-        return tempStr
+    def resizeEvent(self, QResizeEvent):
+        super(TitleBar, self).resizeEvent(QResizeEvent)
+        self.title.setFixedWidth(self.parent.width())
 
-class Example(QWidget):
-    updateList = []
+    def mousePressEvent(self, event):
+        self.start = self.mapToGlobal(event.pos())
+        self.pressing = True
 
-    def __init__(self):
-        super().__init__()
-        self.initUI()
+    def mouseMoveEvent(self, event):
+        if self.pressing:
+            self.end = self.mapToGlobal(event.pos())
+            self.movement = self.end - self.start
+            self.parent.setGeometry(self.mapToGlobal(self.movement).x(),
+                                    self.mapToGlobal(self.movement).y(),
+                                    self.parent.width(),
+                                    self.parent.height())
+            self.start = self.end
+
+    def mouseReleaseEvent(self, QMouseEvent):
+        self.pressing = False
+
+    def btn_close_clicked(self):
+        self.parent.close()
 
 
-    def initUI(self):
-        self.pListView = ListView()
-        self.pListView.setViewMode(QListView.ListMode)
-        self.pListView.setStyleSheet("QListView{icon-size:120px}")
-
-        self.btn_tester = QPushButton("tester", self)
-        self.btn_addmore = QPushButton("Add More", self)
-        # self.btn_remove = QPushButton("Remove", self)
-        self.btn_removeall = QPushButton("Remove All", self)
-
-        Hbox = QHBoxLayout()
-        Hbox.addWidget(self.btn_addmore)
-        Hbox.addStretch(1)
-        # Hbox.addWidget(self.btn_remove)
-        Hbox.addWidget(self.btn_removeall)
-
-        Vbox = QVBoxLayout()
-        Vbox.addLayout(Hbox)
-        Vbox.addWidget(self.pListView)
-        Vbox.addWidget(self.btn_tester)
-        self.setLayout(Vbox)
-        self.setGeometry(300, 300, 360, 720)
-        self.setWindowTitle("Pre-Research")
-        self.setWindowIcon(QIcon('GUI/icon_32.png'))
-        # self.title.setFixedHeight(35)
-        self.btn_tester.clicked.connect(self.tester)
-        self.btn_addmore.clicked.connect(self.addmore)
-        # self.btn_remove.clicked.connect(self.remove)
-        self.btn_removeall.clicked.connect(self.removeall)
-        self.show()
-
-    def addmore(self):
-        print("This is addmore!")
-        filePaths = self.getfilePathList()
-        print("Filepaths", filePaths)
-        files = self.openFileNamesDialog()
-        existFiles = []
-        unsupportedFiles = []
-        msg = ''
-        if files:
-            for file in files:
-                basename = os.path.basename(file)
-                name = basename.split(".")[0]
-                ItemData = {}
-                ItemData['name'] = name
-                ItemKind = pt.getFileKind(file)
-                ItemData['iconPath'] = "GUI/%sThumbnail.png" % ItemKind
-                ItemData['filePath'] = file
-                ItemData['mediaInfo'] = "Reselution FPS\nDuration"
-                # if ItemKind is "video":
-                #     mediaInfo = pt.getMediaInfo(file)
-                #     print(mediaInfo)
-                #     ItemData['mediaInfo']= mediaInfo
-
-                if ItemData['filePath'] in filePaths:
-                    print("\n", basename, "Already in the list!")
-                    existFiles.append(basename)
-                    pass
-                elif ItemKind is None:
-                    unsupportedFiles.append(basename)
-                else:
-                    self.pListView.addItem(ItemData)
-                    self.updateList.append(file)
-        if existFiles:
-            tempmsg = str(existFiles).replace('[', '').replace(']', '').replace(',', '\n').replace(' ', '').replace(
-                '\'', '')
-            msg = msg + '\n' + tempmsg[:300] + "...\nAlready in the list.\n...\n"
-        if unsupportedFiles:
-            tempmsg = str(unsupportedFiles).replace('[', '').replace(']', '').replace(',', '\n').replace(' ',
-                                                                                                         '').replace(
-                '\'', '')
-            msg = msg + '\n' + tempmsg[:300] + "...\nNot Supported.\n...\n"
-        if msg:
-            print(msg)
-            QMessageBox.about(self, 'Files already in the list', msg)
-        if self.updateList:
-            self.backend = BackendThread()
-            self.backend.update_date.connect(self.updateThumb)
-            self.backend.start()
-            pass
-
-    def updateThumb(self,filePath,iconPath,mediaInfo):
-        print(filePath)
-        print(iconPath)
-        print('\n')
-        for i in self.pListView.m_pModel.ListItemData:
-            if i['filePath'] == filePath:
-                i['iconPath'] = iconPath
-                i['mediaInfo'] = mediaInfo
-                break
-        self.supdate()
-
-    def remove(self):
-        print("This is remove22222222222!")
-        self.pListView.removeItem(0)
-        self.supdate()
-
-    def removeall(self):
-        print("This is removeall!")
-        for i in range(len(self.pListView.m_pModel.ListItemData)):
-            self.pListView.removeItem(0)
-        self.supdate()
-
-    def openFileNamesDialog(self):
-        options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        files, _ = QFileDialog.getOpenFileNames(self, "Please Select Your Footages", "",
-                                                "All Files (*);;Media Files (*.mov,*.mp4,*.avi)", options=options)
-        if files:
-            return files
-
-    def supdate(self):
-        self.setFixedWidth(self.width() + 1)
-        self.setFixedWidth(self.width() - 1)
-
-    def getfilePathList(self):
-        filePaths = []
-        for i in self.pListView.m_pModel.ListItemData:
-            try:
-                filePaths.append(i['filePath'])
-            except:
-                pass
-        return filePaths
-
-    def tester(self):
-        print("This is Tester!")
-        print(self.pListView.m_pModel.ListItemData)
-
-
-
-class BackendThread(QThread):
-    print('BackendThread')
-    print(Example.updateList)
-    update_date = pyqtSignal(str,str,str)
-    def run(self):
-        st = time.time()
-        for filePath in Example.updateList:
-            iconPath = pt.findThumb(filePath,"Temp/Cache")
-            pt.findMediaInfo(filePath, "Temp/Cache")
-            width, height, fps, duration = pt.findMediaInfo(filePath,"Temp/Cache")
-            mediaInfo = "%(width)d x %(height)d  %(fps).02f\n%(duration)s" % {'width':width, 'height':height, 'fps':fps, 'duration':duration}
-            # print(mediaInfo)
-            self.update_date.emit(filePath, iconPath, mediaInfo)
-        et = time.time()
-        print("Use Time to Load: %.4f\n"%(et-st))
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    # shutil.rmtree('Temp')
-    ex = Example()
-    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("ex")
+    mw = MainWindow()
+    mw.show()
     sys.exit(app.exec_())
